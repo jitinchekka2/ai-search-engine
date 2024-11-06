28696f9deff229ff303d229de808f0f5f6728360
Jitin Chekka <cjkrishna@aqfer.com>
2024-11-06 12:52:01 +0530

    changed config

--- app/config.tsx
+++ app/config.tsx
@@ -8,13 +8,13 @@ export const config = {
     useOllamaInference: false,
     useOllamaEmbeddings: false,
     searchProvider: 'serper', // 'serper', 'google', 'brave' // 'serper' is the default
-    inferenceModel: 'llama-3.1-70b-versatile', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama3' etc
+    inferenceModel: 'gpt-3.5-turbo', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama3' etc
     inferenceAPIKey: process.env.GROQ_API_KEY, // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
     embeddingsModel: 'text-embedding-3-small', // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
     textChunkSize: 800, // Recommended to decrease for Ollama
     textChunkOverlap: 200, // Recommended to decrease for Ollama
     numberOfSimilarityResults: 4, // Number of similarity results to return per page
-    numberOfPagesToScan: 10, // Recommended to decrease for Ollama
+    numberOfPagesToScan: 7, // Recommended to decrease for Ollama
     nonOllamaBaseURL: 'https://api.groq.com/openai/v1', //Groq: https://api.groq.com/openai/v1 // OpenAI: https://api.openai.com/v1 
     useFunctionCalling: true, // Set to true to enable function calling and conditional streaming UI (currently in beta)
     useSemanticCache: false, // Uses Upstash semantic cache to store and retrieve data for faster response times
